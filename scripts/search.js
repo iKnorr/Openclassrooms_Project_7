397c46801f76e175474db6af4a97ccd679061591
Ingo Knorr <ingoknorr@gmail.com>
2023-03-30 22:20:29 +0200

    commit

--- scripts/search.js
+++ scripts/search.js
@@ -1,14 +1,35 @@
-const searchBar = document.getElementById('search-bar');
+// Alternative with for loop
+// let filteredWithFor = [];
+
+// for (i = 0; i < recipes.length; i++) {
+//   if (
+//     recipes[i].name.toLowerCase().includes('huile') ||
+//     recipes[i].description.toLowerCase().includes('huile') ||
+//     recipes[i].ingredients.some(i => i.ingredient.toLowerCase().includes('huile'))
+//   ) {
+//     filteredWithFor = [...filteredWithFor, recipes[i]];
+//   }
+// }
+// console.log('FOR LOOP', filteredWithFor);
+
+// const showInputValue = () => {
+//   inputValue.innerHTML = `${searchTerm}`;
+// };
+const searchBar = document.getElementById('search-bar-common');
 const results = document.getElementById('results');
 const inputValue = document.getElementById('input-value');
 const inputWrapper = document.querySelector('.input-value-wrapper');
 const closeIcon = document.querySelector('.fa-circle-xmark');
 const recipesNotFound = document.querySelector('.recipe-not-found');
-let searchTerm = '';
+const ingredientsSearch = document.querySelector('.ingredients-search');
+const ingredientsListUL = document.querySelector('.ingredients-list');
+const indexIngredients = document.querySelector('.index-ingredients');
 
 let recipes;
 
 getData().then(recipes => {
+  let searchTerm = '';
+  let searchTermIngredients = '';
   // closeIcon.addEventListener('click', () => {
   //   console.log(searchBar.value);
   //   searchTerm = '';
@@ -22,126 +43,59 @@ getData().then(recipes => {
     searchTerm = e.target.value.toLowerCase();
     if (searchTerm.length >= 3) {
       showList();
+    } else if (searchTerm.length === 0) {
+      showList();
     }
-    showList();
   });
 
-  // Alternative with for loop
-  // let filteredWithFor = [];
+  ingredientsSearch.addEventListener('input', e => {
+    searchTermIngredients = e.target.value.toLowerCase();
+    if (searchTermIngredients.length >= 3) {
+      showList();
+    } else if (searchTermIngredients.length === 0) {
+      showList();
+    }
+  });
 
-  // for (i = 0; i < recipes.length; i++) {
-  //   if (
-  //     recipes[i].name.toLowerCase().includes('huile') ||
-  //     recipes[i].description.toLowerCase().includes('huile') ||
-  //     recipes[i].ingredients.some(i => i.ingredient.toLowerCase().includes('huile'))
-  //   ) {
-  //     filteredWithFor = [...filteredWithFor, recipes[i]];
-  //   }
-  // }
-  // console.log('FOR LOOP', filteredWithFor);
+  const btnIngredients = document.querySelector('.btn-ingredients');
+  const chevronUp = document.querySelector('.fa-chevron-up');
 
-  // const showInputValue = () => {
-  //   inputValue.innerHTML = `${searchTerm}`;
-  // };
+  btnIngredients.addEventListener('click', () => {
+    ingredientsSearch.style.display = 'block';
+    btnIngredients.style.display = 'none';
+  });
 
-  const searchName = (name, searchTerm) => {
-    name.toLowerCase().includes(searchTerm);
-  };
-
-  const searchIngredients = (ingredients, searchTerm) => {
-    ingredients.some(i => i.ingredient.toLowerCase().includes(searchTerm));
-  };
+  chevronUp.addEventListener('click', e => {
+    ingredientsSearch.style.display = 'none';
+    btnIngredients.style.display = 'block';
+  });
 
   const showList = () => {
     results.innerHTML = '';
-    const filteredResults = recipes.filter(({ name, ingredients, description }) => {
-      return (
-        searchName(name, searchTerm) ||
-        searchIngredients(ingredients, searchTerm) ||
-        description.toLowerCase().includes(searchTerm)
-      );
-    });
+    const filteredResults = filterAll(recipes, searchTerm);
 
     if (!!filteredResults && searchTerm.length >= 3) {
       recipesNotFound.innerHTML = `Aucune recette ne correspond à votre critère &#9785;. Vous pouvez
     chercher « coco », « poisson », etc.`;
     }
 
-    filteredResults.forEach(item => {
-      recipesNotFound.innerHTML = ``;
-      const card = document.createElement('div');
+    createCardsDOM(filteredResults);
 
-      card.innerHTML = `
-      <div class="card">
-          <div class="card-fake-img" alt=""></div>
-          <div class="card-body">
-            <div class="card-text">
-              <h5 class="card-title card-name">${item.name}</h5>
-              <div class="card-time card-title">
-                <i class="fa-regular fa-clock"></i>
-                <h5 class="card-time-number">${item.time} min</h5>
-              </div>
-            </div>
-            <div class="card-ingredients-description">
-              <div class="card-ingredients">
-                <div class="ingredients">
-                  <ul>${item.ingredients
-                    .map(i => {
-                      return `<li><b>${i.ingredient}:</b> ${i.quantity ? i.quantity : ''} ${
-                        i.unit ? (i.unit === 'grammes' ? 'g' : i.unit) : ''
-                      }</li>`;
-                    })
-                    .join('')}</ul>
-                </div>
-                <p class="card-text card-text-description">${item.description}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-      results.appendChild(card);
-    });
-  };
+    const ingredientsOnly = filterOnlyIngredients(filteredResults);
 
-  let ingredientsList = [];
-  recipes.map(({ ingredients }) => {
-    return ingredients.map(({ ingredient }) => ingredientsList.push(ingredient.toLowerCase()));
-  });
+    let formerSET;
+    const ingredientsSET = [...new Set(ingredientsOnly)];
+    formerSET = ingredientsSET;
 
-  const ingredientsSET = new Set(ingredientsList);
-  const newIngredientsList = [...ingredientsSET];
+    const filteredIngredients = formerSET.filter(i => i.toLowerCase().includes(searchTermIngredients));
 
-  const ingredientsSearch = document.querySelector('.ingredients-search');
-  const ingredientsWrapper = document.createElement('div');
-  let searchTermIngredients = '';
-
-  ingredientsSearch.addEventListener('input', e => {
-    searchTermIngredients = e.target.value.toLowerCase();
-    if (searchTermIngredients.length >= 3) {
-      showListIngredients();
-      showList();
+    if (ingredientsSearch.textContent.length >= 3) {
+      createIngredientsDOM(filteredIngredients);
+    } else {
+      createIngredientsDOM(ingredientsSET);
     }
-    showListIngredients();
-    showList();
-  });
-
-  const showListIngredients = () => {
-    const filteredIngredients = newIngredientsList.filter(i => i.includes(searchTermIngredients));
-
-    ingredientsWrapper.innerHTML = `
-
-    <i class="fa-solid fa-chevron-up"></i>
-    <ul class="ingredients-list">${filteredIngredients
-      .map(i => {
-        return `<li><a class="ingredients-item" href="#">${i}</a></li>`;
-      })
-      .join('')}
-      </ul>
-      `;
-    ingredientsSearch.appendChild(ingredientsWrapper);
+    createIngredientIndex(filteredResults);
   };
 
-  showListIngredients();
   showList();
 });
---
+++ scripts/services.js
@@ -0,0 +1,97 @@
+const filterAll = (data, term) => {
+  const results = data.filter(({ name, ingredients, description }) => {
+    return (
+      name.toLowerCase().includes(term) ||
+      ingredients.some(i => i.ingredient.toLowerCase().includes(term)) ||
+      description.toLowerCase().includes(term)
+    );
+  });
+  return results;
+};
+
+const filterIngredients = (data, term) => {
+  const results = data.filter(({ ingredients }) => {
+    return ingredients.some(i => i.ingredient.toLowerCase().includes(term));
+  });
+  return results;
+};
+
+const filterOnlyIngredients = data => {
+  let ingredientsList = [];
+  data.map(({ ingredients }) => {
+    return ingredients.map(({ ingredient }) => ingredientsList.push(ingredient.toLowerCase()));
+  });
+  return ingredientsList;
+};
+
+const createCardsDOM = filteredData => {
+  const results = document.getElementById('results');
+  results.innerHTML = '';
+
+  filteredData.forEach(({ name, time, ingredients, description }) => {
+    recipesNotFound.innerHTML = ``;
+    const card = document.createElement('div');
+
+    card.innerHTML = `
+    <div class="card">
+        <div class="card-fake-img" alt=""></div>
+        <div class="card-body">
+          <div class="card-text">
+            <h5 class="card-title card-name">${name}</h5>
+            <div class="card-time card-title">
+              <i class="fa-regular fa-clock"></i>
+              <h5 class="card-time-number">${time} min</h5>
+            </div>
+          </div>
+          <div class="card-ingredients-description">
+            <div class="card-ingredients">
+              <div class="ingredients">
+                <ul>${ingredients
+                  .map(({ ingredient, unit, quantity }) => {
+                    return `<li><b>${ingredient}:</b> ${ingredient ? quantity : ''} ${
+                      unit ? (unit === 'grammes' ? 'g' : unit) : ''
+                    }</li>`;
+                  })
+                  .join('')}</ul>
+              </div>
+              <p class="card-text card-text-description">${description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+    results.appendChild(card);
+  });
+  const numberOfResults = document.querySelector('.number-of-results');
+  numberOfResults.innerHTML = `${filteredData.length} recipes available`;
+};
+
+const createIngredientsDOM = list => {
+  ingredientsListUL.innerHTML = `${list
+    .map(i => {
+      return `<li class="ingredients-item">${i}</li>`;
+    })
+    .join('')}
+    `;
+};
+
+const createIngredientIndex = filteredData => {
+  const ingredientsItems = document.querySelectorAll('.ingredients-item');
+  ingredientsItems.forEach(i => {
+    i.addEventListener('click', e => {
+      let newTerm = e.target.textContent.toLowerCase();
+      const newFiltered = filterIngredients(filteredData, newTerm);
+      const newIngredientsOnly = filterOnlyIngredients(newFiltered);
+
+      const newIngredient = document.createElement('span');
+      newIngredient.classList.add('new-ingredient');
+      newIngredient.innerHTML = `<p>${newTerm}</p><i class="fa-regular fa-circle-xmark"></i>`;
+      indexIngredients.appendChild(newIngredient);
+      console.log(newTerm, newIngredientsOnly, newFiltered);
+
+      createIngredientsDOM(newIngredientsOnly);
+      createCardsDOM(newFiltered);
+    });
+  });
+};
